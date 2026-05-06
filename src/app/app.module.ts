e8ab1623993a8cc00b99bfb576484838335c1b17
Gianmarco Incocciati <g.incocciati.esterno@almaviva.it>
2026-05-06 15:02:16 +0200

    feat: added MatInputModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
-    ProvaComponent
+    ProvaComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +23,7 @@ import {MatInputModule} from '@angular/material/input';
     MatSliderModule,
     MatCardModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
